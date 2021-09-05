2f5c0e3ba436575bbec8301e7e294e98faaebd84
john-hexis <jh.tech.2011@gmail.com>
2021-09-05 13:53:57 +0800

    Fix ViewBuilder
    Add ViewControllerBuilder

---
+++ Sources/UIKitTranceLibrary/Guide/VIPER/ViewControllerBuilderPr.swift
@@ -0,0 +1,16 @@
+//
+//  ViewControllerBuilderPr.swift
+//  
+//
+//  Created by John Harries on 5/9/21.
+//
+
+#if canImport(UIKit)
+import Foundation
+import UIKit
+
+public protocol ViewControllerBuilderPr {
+    associatedtype U: UIViewController
+    func build() -> U
+}
+#endif
